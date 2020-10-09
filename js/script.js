d6f74513e1f9fd14df2ac8c06e3cd8bbd4748797
David Orona <OronaDavidJ@JohnDeere.com>
2020-10-09 15:28:36 -0500

    Initialize class structure of logic

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log(`JS working!`);
+
+class Game {
+    constructor(numberOfRings, numberOfPegs = 3){
+        this.numberOfRings = numberOfRings;
+    }
+}
+
+class Peg {
+    constructor() {
+        this.ringsOnPeg = [];
+    }
+}
+
+class Ring {
+    constructor(size) {
+        this.size = size;
+    }
+}
+
+new Game(3);
