495cad5767b157451b97b5e719707b164bbbad86
David Orona <OronaDavidJ@JohnDeere.com>
2020-10-10 10:15:10 -0500

    Store list of rings. Create function to share with pegs

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 console.log(`JS working!`);
 
 class Game {
-    constructor(numberOfRings, numberOfPegs = 3){
-        this.numberOfRings = numberOfRings;
-        this.listOfPegs = Array.from(Array(numberOfPegs), peg => peg = new Peg());
+    constructor(numberOfRings, numberOfPegs = 3) {
+        this.listOfRings = this.initializeArray(Ring, numberOfRings);
+        this.listOfPegs = this.initializeArray(Peg, numberOfRings);
+    }
+
+    initializeArray(Object, quantity) {
+        return Array.from(Array(quantity), (item, index) => item = new Object(index));
+    }
+
+    movePeg() {
+        
     }
 }
 
 class Peg {
-    constructor() {
+    constructor(pegIndex) {
         this.ringsOnPeg = [];
     }
 
@@ -19,13 +27,23 @@ class Peg {
     removeRing() {
         this.ringsOnPeg.pop();
     }
-
 }
 
 class Ring {
-    constructor(size) {
-        this.size = size;
+    constructor(units) {
+        this.size = units * 25;
+    }
+
+    createNewRing() {
+        const ring = document.createElement(`div`);
+        ring.classList = `ring`;
+        // ring.id = `ring${}`;
+        ring.addEventListener(`click`, this.selectRing);
+    }
+
+    selectRing() {
+        console.log(`Hi`);
     }
 }
 
-new Game(3);
+new Game(5);
