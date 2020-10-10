64799aa1cfea33287491603b47cad66433491698
David Orona <OronaDavidJ@JohnDeere.com>
2020-10-10 18:55:19 -0500

    Create pegs and divs from JS

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ console.log(`JS working!`);
 
 class Game {
     constructor(numberOfRings, numberOfPegs = 3) {
-        this.listOfRings = this.initializeArray(Ring, numberOfRings);
         this.listOfPegs = this.initializeArray(Peg, numberOfPegs);
-
         // this.listOfPegs.forEach(peg => this.createContainer(peg.index));
 
+        this.listOfRings = this.initializeArray(Ring, numberOfRings);
+        
         this.listOfPegs[0].ringsOnPeg = this.listOfRings;
         console.log(this.listOfPegs);
         console.log(this.listOfRings);
@@ -35,6 +35,43 @@ class Peg {
     constructor(pegIndex) {
         this.index = pegIndex;
         this.ringsOnPeg = [];
+        this.createContainer();
+        this.createNewPegElement();
+        this.createRingsContainer();
+    }
+
+    createContainer() {
+        const containers = document.querySelector(`.containers`);
+
+        const container = document.createElement(`div`);
+        container.className = `container`;
+        container.id = `container${this.index}`;
+
+        containers.appendChild(container);
+    }
+
+    createNewPegElement() {
+        const container = document.querySelector(`#container${this.index}`);
+
+        const peg = document.createElement(`div`);
+        peg.classList = `peg`;
+        peg.id = `peg${this.index}`;
+        peg.style.height = `400px`;
+        peg.style.width = `25px`;
+        peg.style.margin = `0 auto`;
+        peg.style.background = `black`;
+        peg.addEventListener(`click`, this.selectRing);
+
+        container.appendChild(peg);
+    }
+
+    createRingsContainer() {
+        const container = document.querySelector(`#container${this.index}`);
+
+        const rings = document.createElement(`div`);
+        rings.classList = `rings`;
+
+        container.appendChild(rings);
     }
 
     addRing(ring) {
@@ -49,18 +86,19 @@ class Peg {
 class Ring {
     constructor(ringIndex) {
         this.index = ringIndex;
-        this.ringElement = this.createNewRingElement();
+        this.createNewRingElement();
     }
 
     createNewRingElement() {
+        const container = document.querySelector(`#container0 .rings`);
+
         const ring = document.createElement(`div`);
         ring.classList = `ring`;
-        ring.id = `ring${this.ringIndex}`;
-        ring.style.width = this.ringIndex * 25;
+        ring.id = `ring${this.index}`;
+        ring.style.width = (5 - this.index) * 25;
         ring.addEventListener(`click`, this.selectRing);
 
-        // const container = document.querySelector(`.container1`);
-        // container.appendChild(ring);
+        container.prepend(ring);
     }
 
     selectRing() {
@@ -68,4 +106,4 @@ class Ring {
     }
 }
 
-new Game(5);
+new Game(3);
